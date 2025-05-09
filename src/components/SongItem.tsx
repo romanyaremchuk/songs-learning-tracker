3d1576aeebd77df74b50d20b590bae9dbb8690b8
roman-y <romanza33@gmail.com>
2025-05-09 16:05:36 +0200

    improvement to item menu so it is displayed on top of items

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -5,14 +5,20 @@ import style from "./SongItem.module.css";
 
 const SongItem = ({ song }: { song: Song }) => {
   return (
-    <div className={style.SongItem}>
-      <p>
-        {song.url.length === 0 ? song.name : <a href={song.url}>{song.name}</a>}
-      </p>
-      <div className={style.SongItemMenu}>
-        <ItemMenu />
+    <>
+      <div className={style.SongItem}>
+        <p>
+          {song.url.length === 0 ? (
+            song.name
+          ) : (
+            <a href={song.url}>{song.name}</a>
+          )}
+        </p>
+        <div className={style.SongItemMenu}>
+          <ItemMenu />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
