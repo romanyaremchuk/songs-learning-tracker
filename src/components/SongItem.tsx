184f7f3d6f18749a75057123707ac6f0fe3b615f
roman-y <romanza33@gmail.com>
2025-05-10 18:53:36 +0200

    improvements to menu display

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Song } from "../types/song";
 import ItemMenu from "./ItemMenu";
 import style from "./SongItem.module.css";
+import useClickOutside from "../hooks/useClickOutside"; // убедись, что хук есть
 
 const SongItem = ({ song }: { song: Song }) => {
+  const [itemMenuShown, setItemMenuShown] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useClickOutside(wrapperRef as React.RefObject<HTMLElement>, () => {
+    setItemMenuShown(false);
+  });
   return (
-    <>
-      <div className={style.SongItem}>
-        <p>
-          {song.url.length === 0 ? (
-            song.name
-          ) : (
-            <a href={song.url}>{song.name}</a>
-          )}
-        </p>
-        <div className={style.SongItemMenu}>
+    <div className={style.SongItem}>
+      <p>
+        {song.url.length === 0 ? song.name : <a href={song.url}>{song.name}</a>}
+      </p>
+      {itemMenuShown ? (
+        <div ref={wrapperRef} className={style.SongItemMenu}>
           <ItemMenu />
         </div>
-      </div>
-    </>
+      ) : (
+        <div className={style.SongShowItemMenuButton}>
+          <button onClick={() => setItemMenuShown(true)}>Show menu</button>
+        </div>
+      )}
+    </div>
   );
 };
 
