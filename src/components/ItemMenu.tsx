92f467c6ec043842b1b888a2fbe8399f0e8d859a
roman-y <romanza33@gmail.com>
2025-05-10 19:12:44 +0200

    css border radius to make it more pretty

--- src/components/ItemMenu.tsx
+++ src/components/ItemMenu.tsx
@@ -4,8 +4,8 @@ import style from "./ItemMenu.module.css";
 const ItemMenu = () => {
   return (
     <div className={style.ItemMenu}>
-      <button>Delete</button>
-      <button>Remove</button>
+      <button className={style.ItemMenuButton}>Delete</button>
+      <button className={style.ItemMenuButton}>Remove</button>
     </div>
   );
 };
