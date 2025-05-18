dcee593b88ecb479b7bea274a64cb5b563c73a5c
roman-y <romanza33@gmail.com>
2025-05-18 23:13:05 +0200

    wokring on form

--- src/components/SongForm.tsx
+++ src/components/SongForm.tsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Song } from "../types/song";
 
-const SongForm = () => {
-  //const [filter, setFilter] = useState("all");
+interface Props {
+  songs: Song[];
+  setSongs: (songs: Song[]) => void;
+}
+
+const SongForm = ({ songs, setSongs }: Props) => {
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const [status, setStatus] = useState("wantToLearn");
+  const [newSong, setNewSong] = useState<Song>();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setNewSong({ id: uuidv4(), url: "", name: "", status: status });
+  };
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <label>
         {" "}
-        Song name: <input name="inputSongName" />{" "}
+        Song name:{" "}
+        <input
+          value={newSong?.name}
+          onChange={(e) => setName(e.target.value)}
+          name="inputSongName"
+        />{" "}
       </label>
       <label>
         {" "}
-        URL: <input name="inputURL" />{" "}
+        URL: <input
+          onChange={(e) => setUrl(e.target.value)}
+          name="inputURL"
+        />{" "}
       </label>
-      <button type="submit">Add song</button>
       <label>
         <select>
-          <option>I want to learn</option>
-          <option>I am learning</option>
-          <option>I have learned</option>
+          <option onClick={() => setStatus("wantToLearn")}>
+            I want to learn
+          </option>
+          <option onClick={() => setStatus("learning")}>I am learning</option>
+          <option onClick={() => setStatus("learned")}>I have learned</option>
         </select>
       </label>
-    </>
+      <button type="submit">Add song</button>
+    </form>
   );
 };
 
 export default SongForm;
+function uuidv4(): string {
+  throw new Error("Function not implemented.");
+}
